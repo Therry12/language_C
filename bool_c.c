1dcd56cda071164c47b5082e8736037010cc4df5
Therry12 <incompsince@gmail.com>
2024-06-27 16:01:39 +0300

    Add bool.c

---
+++ bool_c.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+  // int digit;
+  // scanf("%d", &digit);
+  // char even = digit % 2 == 0;
+  // if (even)
+  //   printf("Number %d is even\n", digit);
+  // else
+  //   printf("Number %d not even\n", digit);
+
+  //---------------------then-----------------------------
+
+  int inputNum, inputMode, inputMinRange, inputMaxRange;
+  printf("Input number:\n");
+  scanf("%d", &inputNum);
+
+  printf("Enter range, a.k.a min max \n");
+  scanf("%d %d", &inputMinRange, &inputMaxRange);
+
+
+  char isRange = inputNum >= inputMinRange && inputNum <= inputMaxRange;
+  char isNotRange = !isRange;  // same thing y < - 2 || y > 5
+
+
+  printf("Enter mode\n[1]Spot in range\n[0]Spot if isnt in range\n");
+  scanf("%d", &inputMode);
+
+  if (inputMode)
+    printf("Num %d in range\n", inputNum);
+  else
+    printf("Num %d isnt in range", inputNum);
+
+  return 0;
+}

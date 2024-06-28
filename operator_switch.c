8168820dd35cba05b08e11c2e47f045dd55f6f57
Therry12 <incompsince@gmail.com>
2024-06-28 19:27:59 +0300

    Added comments

--- operator_switch.c
+++ operator_switch.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
 int main() {
+  // -------------------Menu------------------------------------
   int currentMenu = 0;
 
-  printf("[1]C++\n[2]C\n[3]Python\n[4]Java\nSelect menu: ");
+  printf("[1]C++\n[2]C\n[3]Python\n[4]Java\n[5]Exit\nSelect menu: ");
   if (scanf("%d", &currentMenu) != 1) {
     printf("Error!Enter valid number!\n");
     return 1;
@@ -11,21 +12,25 @@ int main() {
     switch (currentMenu) {
     case 1:
       printf("Selected C++!\n");
-      break;
+      return 0; // TODO: Use return 0; instead break if program finishes after
+                // switch
     case 2:
       printf("Selected C!\n");
-      break;
+      return 0;
     case 3:
       printf("Selected Python!\n");
-      break;
+      return 0;
     case 4:
       printf("Selected Java!\n");
-      break;
+      return 0;
+    case 5:
+      printf("Exit!\n");
+      return 0;
     default:
       printf("Error! Enter valid number!\n");
-      break;
-      return 1;
+      return 0;
     }
   }
+
   return 0;
 }

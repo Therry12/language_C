fb94ee4355061ab16e2c6135529f6361de459d85
Therry12 <incompsince@gmail.com>
2024-07-08 13:52:13 +0300

    Minor changes

--- practice_c/pointers_r_ezz.c
+++ practice_c/pointers_r_ezz.c
@@ -21,15 +21,38 @@ int main() {
   //
   // printf("p_arg = %d, p = %d\n", *p_arg, *p);
   // return 0
-  int *p;
-  *p = 10; // BAD: Don't use it bad practice
+  // int *p;
+  // *p = 10; // BAD: Don't use it bad practice
+  //
+  // int arg = 12;
+  // int *bp = NULL;
+  //
+  // if (bp != NULL) { // Better: we can use it but its not work every time
+  //   *bp = 25;
+  // }
+  //
+  // printf("%d\n", *p);
+  //
 
-  int arg = 12;
-  int *bp = NULL;
+  // int var = 5;
+  // int *ptr = &var;
+  //
+  // printf("addres var = %u\n", ptr);
+  // ptr++;
+  // printf("addres var = %u\n", ptr);
 
-  if (bp != NULL) { // Better: we can use it but its not work every time
-    *bp = 25;
+  // int ar[10];
+  // int *ptr = ar;
+  // int *p = &ar[3];
+  //
+  // int difer = p - ptr;
+  // printf("ar = %u, ptr = %u, p = %u, difer = %d\n", ar, ptr, p, difer);
+
+  int g = 767676;
+  char *ptrg = (char *)&g; // its = char *ptr = &g but it cast
+
+  for (int i = 0; i < sizeof(g); ++i) {
+    printf("%d\t", *ptrg++);
   }
-
-  printf("%d\n", *p);
+  printf("\n");
 }
